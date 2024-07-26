f289f00a5a5edaa3f2187d94596fa0223935fbac
Arpit trivedi <arpittrivedi@gmail.com>
2024-07-26 13:18:22 +0530

    Your commit message

--- program1.js
+++ program1.js
@@ -1,8 +1,38 @@
 const getTotalIsles = function (grid) {
+  if  (grid === null || grid.length === 0) {
+      return 0;
+  }
 
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let numIsles = 0;
 
-  // write your code here
+  function dfs(row, col) {
+     
+      if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] === 'W') {
+          return;
+      }
 
+      
+      grid[row][col] = 'W';
+
+      
+      dfs(row - 1, col); 
+      dfs(row + 1, col); 
+      dfs(row, col - 1); 
+      dfs(row, col + 1); 
+  }
+
+  for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+          if (grid[row][col] === 'L') {
+              numIsles++;
+              dfs(row, col);
+          }
+      }
+  }
+
+  return numIsles;
 };
 
 module.exports = getTotalIsles;
